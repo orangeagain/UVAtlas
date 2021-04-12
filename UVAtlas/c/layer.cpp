7d13b08e25632adf6e2cbdc7ff3acff980b053a7
Amer Koleci <amerkoleci@gmail.com>
2021-04-12 11:08:36 +0200

    Expose some DirectXMesh functions to c layer and FIX UVAtlasCreate invoke.

--- UVAtlas/c/layer.cpp
+++ UVAtlas/c/layer.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include "DirectXMesh.h"
 #include "UVAtlas.h"
 
 #if defined(_MSC_VER)
@@ -10,6 +11,63 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+    /* DirectXMesh */
+    EXPORT_C_API HRESULT GenerateAdjacencyAndPointReps_UInt16(
+        const uint16_t* indices, size_t nFaces,
+        const DirectX::XMFLOAT3* positions, _In_ size_t nVerts,
+        float epsilon,
+        uint32_t* pointRep,
+        uint32_t* adjacency)
+    {
+        return DirectX::GenerateAdjacencyAndPointReps(
+            indices, nFaces,
+            positions, nVerts,
+            epsilon,
+            pointRep,
+            adjacency);
+    }
+
+    EXPORT_C_API HRESULT GenerateAdjacencyAndPointReps_UInt32(
+        const uint32_t* indices, size_t nFaces,
+        const DirectX::XMFLOAT3* positions, _In_ size_t nVerts,
+        float epsilon,
+        uint32_t* pointRep,
+        uint32_t* adjacency)
+    {
+        return DirectX::GenerateAdjacencyAndPointReps(
+            indices, nFaces,
+            positions, nVerts,
+            epsilon,
+            pointRep,
+            adjacency);
+    }
+
+    typedef enum ComputeNormalsFlags {
+        CNORM_DEFAULT = DirectX::CNORM_DEFAULT,
+        CNORM_WEIGHT_BY_AREA = DirectX::CNORM_WEIGHT_BY_AREA,
+        CNORM_WEIGHT_EQUAL = DirectX::CNORM_WEIGHT_EQUAL,
+        CNORM_WIND_CW = DirectX::CNORM_WIND_CW,
+        __FORCE_U32 = 0x7FFFFFFF
+    } ComputeNormalsFlags;
+
+    EXPORT_C_API HRESULT ComputeNormals_UInt16(
+        const uint16_t* indices, size_t nFaces,
+        const DirectX::XMFLOAT3* positions, size_t nVerts,
+        ComputeNormalsFlags flags,
+        DirectX::XMFLOAT3* normals)
+    {
+        return DirectX::ComputeNormals(indices, nFaces, positions, nVerts, (DirectX::CNORM_FLAGS)flags, normals);
+    }
+
+    EXPORT_C_API HRESULT ComputeNormals_UInt32(
+        const uint32_t* indices, size_t nFaces,
+        const DirectX::XMFLOAT3* positions, size_t nVerts,
+        ComputeNormalsFlags flags,
+        DirectX::XMFLOAT3* normals)
+    {
+        return DirectX::ComputeNormals(indices, nFaces, positions, nVerts, (DirectX::CNORM_FLAGS)flags, normals);
+    }
 
     typedef enum uvatlas_options {
         UVATLAS_DEFAULT = 0x00,
@@ -30,14 +88,16 @@ extern "C" {
         uint32_t            verticesCount;
         uvatlas_vertex*     vertices;
         uint32_t            indicesCount;
-        uint8_t*            indices;
+        uint32_t*           indices;
+        uint32_t*           facePartitioning;
+        uint32_t*           vertexRemapArray;
         float               stretch;
         size_t              charts;
     } uvatlas_result;
 
-    EXPORT_C_API uvatlas_result* uvatlas_create(
+    EXPORT_C_API uvatlas_result* uvatlas_create_uint32(
         const DirectX::XMFLOAT3* positions, size_t nVerts,
-        const void* indices, bool is32Bit, size_t nFaces,
+        const uint32_t* indices, size_t nFaces,
         size_t maxChartNumber, float maxStretch,
         size_t width, size_t height,
         float gutter,
@@ -56,7 +116,7 @@ extern "C" {
         std::vector<uint32_t> vertexRemapArray;
 
         *result = DirectX::UVAtlasCreate(positions, nVerts,
-            indices, is32Bit ? DXGI_FORMAT_R32_UINT : DXGI_FORMAT_R16_UINT, nFaces,
+            indices, DXGI_FORMAT_R32_UINT, nFaces,
             maxChartNumber, maxStretch,
             width, height,
             gutter,
@@ -78,12 +138,18 @@ extern "C" {
             return nullptr;
         }
 
+        auto newIB = reinterpret_cast<const uint32_t*>(&vMeshOutIndexBuffer.front());
+
         atlas_result->verticesCount = static_cast<uint32_t>(vMeshOutVertexBuffer.size());
-        atlas_result->indicesCount = static_cast<uint32_t>(vMeshOutIndexBuffer.size());
+        atlas_result->indicesCount = static_cast<uint32_t>(nFaces * 3);
         atlas_result->vertices = new uvatlas_vertex[vMeshOutVertexBuffer.size()];
-        atlas_result->indices = new uint8_t[vMeshOutIndexBuffer.size()];
+        atlas_result->indices = new uint32_t[atlas_result->indicesCount];
+        atlas_result->facePartitioning = new uint32_t[vMeshOutVertexBuffer.size()];
+        atlas_result->vertexRemapArray = new uint32_t[vMeshOutVertexBuffer.size()];
         memcpy(atlas_result->vertices, vMeshOutVertexBuffer.data(), vMeshOutVertexBuffer.size());
-        memcpy(atlas_result->indices, vMeshOutIndexBuffer.data(), vMeshOutIndexBuffer.size());
+        memcpy(atlas_result->indices, newIB, atlas_result->indicesCount * sizeof(uint32_t));
+        memcpy(atlas_result->facePartitioning, facePartitioning.data(), facePartitioning.size());
+        memcpy(atlas_result->vertexRemapArray, vertexRemapArray.data(), vertexRemapArray.size());
         return atlas_result;
     }
 
@@ -91,8 +157,21 @@ extern "C" {
     {
         delete[] result->vertices;
         delete[] result->indices;
+        delete[] result->facePartitioning;
+        delete[] result->vertexRemapArray;
         delete result;
         result = nullptr;
+    }
+
+    EXPORT_C_API HRESULT UVAtlasApplyRemap(
+        const void* vbin,
+        size_t stride,
+        size_t nVerts,
+        size_t nNewVerts,
+        const uint32_t* vertexRemap,
+        void* vbout)
+    {
+        return DirectX::UVAtlasApplyRemap(vbin, stride, nVerts, nNewVerts, vertexRemap, vbout);
     }
 
 #ifdef __cplusplus
